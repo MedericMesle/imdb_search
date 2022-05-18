f71b6874b79d0cfd8b2d2def55220a0d5da61790
mederic mesle <mederic.mesle@gmail.com>
2022-05-18 17:05:40 +0200

    Syllabus not working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
   def index
-    if params[:query].present?
-      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
-    else
-      @movies = Movie.all
-    end
+      if params[:query].present?
+          @movies = Movie.where("title @@ :query OR syllabus @@ :query", query: "%#{params[:query]}%")
+      else
+          @movies = Movie.all
+      end
   end
 end
