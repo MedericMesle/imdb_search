e6fdff99eadae1a0cf355744079a7acc4b3a1000
mederic mesle <mederic.mesle@gmail.com>
2022-05-19 13:50:38 +0200

    Searching through table joins

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
   def index
-      if params[:query].present?
-          @movies = Movie.where("title @@ :query OR synopsis @@ :query", query: "%#{params[:query]}%")
+    if params[:query].present?
+        sql_query = "\
+          movies.title ILIKE :query \
+          OR movies.synopsis ILIKE :query \
+          OR directors.first_name ILIKE :query \
+          OR directors.last_name ILIKE :query \
+          "
+          @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
       else
           @movies = Movie.all
       end
