0ba87015af4f9a62320d1af07e4f368645b572c6
mederic mesle <mederic.mesle@gmail.com>
2022-05-19 13:40:08 +0200

    multiple term search fixed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
       if params[:query].present?
-          @movies = Movie.where("title @@ :query OR syllabus @@ :query", query: "%#{params[:query]}%")
+          @movies = Movie.where("title @@ :query OR synopsis @@ :query", query: "%#{params[:query]}%")
       else
           @movies = Movie.all
       end
