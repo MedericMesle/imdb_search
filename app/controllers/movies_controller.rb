2d742db3dbdc63fc939f0329ead6dca5a8f73556
mederic mesle <mederic.mesle@gmail.com>
2022-05-19 14:18:15 +0200

    pg_search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-        sql_query = "\
-          movies.title ILIKE :query \
-          OR movies.synopsis ILIKE :query \
-          OR directors.first_name ILIKE :query \
-          OR directors.last_name ILIKE :query \
-          "
-          @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
-      else
-          @movies = Movie.all
-      end
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
+    else
+      @movies = Movie.all
+    end
   end
 end
